4b11473424da044a4345f0569ffadfd768e4e694
DiegoFatVega <fatvegadiego@gmail.com>
2025-09-03 18:39:07 +0200

    Add express as a dependency in package.json, then tried with terminal the server

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const  app = express();
+const app = express();
 const port = 3000;
 
+app.listen(port, () => {
+    console.log(`example app listening on port http://localhost:${port}`);
+})
+
 app.get('/',(req, res) => {
-    
+    res.send(`<h1> Server del mio Blog </h1>`);
 } )
